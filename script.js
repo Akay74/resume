cb18fe1f73cef9a132a987ec7327c3f15814e4ab
Akay <akaudeh@gmail.com>
2021-08-19 18:55:10 +0100

    modified 4 files

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
+
+
 const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav')
+const mobile_menu = document.querySelector('.mobile-nav');
 
 menu_btn.addEventListener('click', function(){
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 });
-
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $(".navbar-fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-      });
-  });
